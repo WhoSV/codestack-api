f7329f3e6384cb8c2dcad96858ac5fd22b30bc3e
WhoSV <vladislavsiumbeli@gmail.com>
2018-03-23 16:44:01 +0200

    feat implement authorization method

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
+	"strings"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -26,9 +28,10 @@ const (
 type Person struct {
 	ID       uint   `json:"id,omitempty" gorm:"primary_key"`
 	FullName string `json:"full_name,omitempty"`
-	Mail     string `json:"mail,omitempty"`
+	Email    string `json:"email,omitempty" gorm:"unique,not null"`
 	Role     string `json:"role,omitempty"`
 	Password string `json:"password,omitempty"`
+	Token    string `json:"token"`
 }
 
 type ErrorMsg struct {
@@ -42,7 +45,7 @@ func GetPeople(w http.ResponseWriter, r *http.Request) {
 	var people []Person
 
 	if err := db.Find(&people).Error; err != nil {
-		fmt.Printf("can not get all people from db: %v", err)
+		fmt.Printf("can not get all people from db: %v\n", err)
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(ErrorMsg{"can not get all people from db"})
@@ -64,7 +67,7 @@ func GetPerson(w http.ResponseWriter, r *http.Request) {
 	if id := params["id"]; len(id) > 0 {
 		id, err := strconv.Atoi(id)
 		if err != nil {
-			fmt.Printf("can not convert from string to int: %v", err)
+			fmt.Printf("can not convert from string to int: %v\n", err)
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(ErrorMsg{"json decode failed"})
@@ -73,13 +76,13 @@ func GetPerson(w http.ResponseWriter, r *http.Request) {
 
 		q := db.First(&person, id)
 		if q.RecordNotFound() {
-			fmt.Printf("record not found: %v", err)
+			fmt.Printf("record not found: %v\n", err)
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusNotFound)
 			json.NewEncoder(w).Encode(ErrorMsg{"record not found"})
 			return
 		} else if q.Error != nil {
-			fmt.Printf("can not convert from string to int: %v", err)
+			fmt.Printf("can not convert from string to int: %v\n", err)
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(ErrorMsg{"json decode failed"})
@@ -99,7 +102,7 @@ func CreatePerson(w http.ResponseWriter, r *http.Request) {
 	var person Person
 
 	if err := json.NewDecoder(r.Body).Decode(&person); err != nil {
-		fmt.Printf("json decode failed: %v", err)
+		fmt.Printf("json decode failed: %v\n", err)
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(ErrorMsg{"json decode failed"})
@@ -108,12 +111,13 @@ func CreatePerson(w http.ResponseWriter, r *http.Request) {
 
 	// Create new person in DB.
 	if err := db.Create(&person).Error; err != nil {
-		fmt.Printf("person creation failed: %v", err)
+		fmt.Printf("person creation failed: %v\n", err)
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(ErrorMsg{"person creation failed"})
 		return
 	}
+	// TODO(vlad): Handle the case where user email is not unique.
 
 	w.WriteHeader(http.StatusCreated)
 }
@@ -128,7 +132,7 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {
 	if id := params["id"]; len(id) > 0 {
 		id, err := strconv.Atoi(id)
 		if err != nil {
-			fmt.Printf("can not convert from string to int: %v", err)
+			fmt.Printf("can not convert from string to int: %v\n", err)
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(ErrorMsg{"json decode failed"})
@@ -137,13 +141,13 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {
 
 		q := db.First(&person, id)
 		if q.RecordNotFound() {
-			fmt.Printf("record not found: %v", err)
+			fmt.Printf("record not found: %v\n", err)
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusNotFound)
 			json.NewEncoder(w).Encode(ErrorMsg{"record not found"})
 			return
 		} else if q.Error != nil {
-			fmt.Printf("can not convert from string to int: %v", err)
+			fmt.Printf("can not convert from string to int: %v\n", err)
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(ErrorMsg{"json decode failed"})
@@ -151,7 +155,7 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if err := db.Delete(&person).Error; err != nil {
-			fmt.Printf("can not delete person: %v", err)
+			fmt.Printf("can not delete person: %v\n", err)
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(ErrorMsg{"can not delete person"})
@@ -161,6 +165,101 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusNoContent)
+}
+
+// Authorization Handler
+func Authorize(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	// Decode request body into data.
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		fmt.Printf("json decode failed: %v\n", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorMsg{"json decode failed"})
+		return
+	}
+
+	// Validate user provided email and password.
+	if len(data.Email) == 0 {
+		err := fmt.Errorf("email can not be empty")
+		fmt.Printf("validation failed: %v\n", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorMsg{"validation failed"})
+		return
+	}
+	if len(data.Password) == 0 {
+		err := fmt.Errorf("password can not be empty")
+		fmt.Printf("validation failed: %v\n", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorMsg{"validation failed"})
+		return
+	}
+
+	// Fetch user by email address provided.
+	var person Person
+
+	q := db.Where(Person{Email: data.Email}).First(&person)
+	if q.RecordNotFound() {
+		fmt.Printf("record not found\n")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(ErrorMsg{"invalid email or password"})
+		return
+	} else if q.Error != nil {
+		fmt.Printf("unknown database error: %v\n", q.Error)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorMsg{"unknown database error"})
+		return
+	}
+
+	// Check if the password is correct.
+	if data.Password != person.Password {
+		fmt.Printf("invalid password\n")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(ErrorMsg{"invalid email or password"})
+		return
+	}
+
+	// Create an authorization token.
+	var token string
+
+	randUUID, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Printf("unknown uuid generation error: %v\n", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorMsg{"unknown uuid generation error"})
+		return
+	}
+	token = randUUID.String()
+
+	// Store creted token in DB.
+	person.Token = token
+	if err := db.Save(&person).Error; err != nil {
+		fmt.Printf("unknown database error: %v\n", q.Error)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorMsg{"unknown database error"})
+		return
+	}
+
+	// Success
+	var resp struct {
+		Token string `json:"token"`
+	}
+	resp.Token = token
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(&resp)
 }
 
 // HandleCORS is a CORS handler.
@@ -194,15 +293,64 @@ func CORSMiddleware(handler http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func TokenAuthMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		s := strings.Split(r.Header.Get("Authorization"), " ")
+
+		if len(s) != 2 {
+			fmt.Printf("malformed authorization token\n")
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(ErrorMsg{"Authorization token should be in the form of Authorization: Bearer <token>"})
+			return
+		}
+
+		if s[0] != "Bearer" {
+			fmt.Printf("authorization token is not bearer kind\n")
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(ErrorMsg{"Authorization token should be in the form of Authorization: Bearer <token>"})
+			return
+		}
+
+		token := s[1]
+
+		// Check token
+		var person Person
+
+		q := db.Where(Person{Token: token}).First(&person)
+		if q.RecordNotFound() {
+			fmt.Printf("not authorized\n")
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(ErrorMsg{"Not Authorized"})
+			return
+		} else if q.Error != nil {
+			fmt.Printf("unknown database error: %v\n", q.Error)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(ErrorMsg{"unknown database error"})
+			return
+		}
+
+		// Permitted
+		h.ServeHTTP(w, r)
+	}
+}
+
 // main function to boot up everything
 func main() {
 	defer db.Close()
 	router := mux.NewRouter()
 
-	router.HandleFunc("/people", CORSMiddleware(GetPeople)).Methods("OPTIONS", "GET")
-	router.HandleFunc("/people/{id}", CORSMiddleware(GetPerson)).Methods("OPTIONS", "GET")
+	// Authorization
+	router.HandleFunc("/auth", CORSMiddleware(Authorize)).Methods("OPTIONS", "POST")
+
+	// Users
+	router.HandleFunc("/people", CORSMiddleware(TokenAuthMiddleware(GetPeople))).Methods("OPTIONS", "GET")
+	router.HandleFunc("/people/{id}", CORSMiddleware(TokenAuthMiddleware(GetPerson))).Methods("OPTIONS", "GET")
 	router.HandleFunc("/people", CORSMiddleware(CreatePerson)).Methods("OPTIONS", "POST")
-	router.HandleFunc("/people/{id}", CORSMiddleware(DeletePerson)).Methods("OPTIONS", "DELETE")
+	router.HandleFunc("/people/{id}", CORSMiddleware(TokenAuthMiddleware(DeletePerson))).Methods("OPTIONS", "DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
